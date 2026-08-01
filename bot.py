88671a2fbdb97fcbef1f5ce0047e09b4c1ea3ed3
kosxfrfre <koskos111@icloud.com>
2026-08-01 16:13:49 +0300

    Replace placeholder token with actual bot token
    Updated the bot token with the actual token value.

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "ВСТАВЬ_СЮДА_ТОКЕН_ОТ_BOTFATHER"
+TOKEN = 8678805338:AAFxrQBnfcGdPUmzWmgJxjqvP2a5K9NmcPA
+
+8678805338:AAFxrQBnfcGdPUmzWmgJxjqvP2a5K9NmcPA
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("🚀 PlanenNews Bot работает!")
