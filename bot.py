2d1ef91059a5b48ac5a5a0891ee75e6fad0a776a
kosxfrfre <koskos111@icloud.com>
2026-08-02 10:08:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,12 +3,11 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "8678805338:AAFxrQBnfcGdPUmzWmgJxjqvP2a5K9NmcPA"
 
-8678805338:AAFxrQBnfcGdPUmzWmgJxjqvP2a5K9NmcPA
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🚀 PlanenNews Bot работает!")
+    await update.message.reply_text("🚀 PlanenNews_Bot запущен!")
 
 app = ApplicationBuilder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
