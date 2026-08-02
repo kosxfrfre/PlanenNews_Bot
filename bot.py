bdfe46b7fc24739b06055191fc430cb2d5220b9c
kosxfrfre <koskos111@icloud.com>
2026-08-02 10:42:22 +0300

    Update bot.py Fix bot

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 TOKEN = "8678805338:AAFxrQBnfcGdPUmzWmgJxjqvP2a5K9NmcPA"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🚀 PlanenNews_Bot запущен!")
+    await update.message.reply_text("🚀 PlanenNews Bot работает!")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
