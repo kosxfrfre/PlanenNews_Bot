88c25428fa681d1de5dc7e6fcfb73bae999680b8
kosxfrfre <koskos111@icloud.com>
2026-08-03 19:56:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telegram.ext import (
 )
 
 TOKEN = "8678805338:AAHPumjkRMOvkd81aPJ_H7kjiVJWi18V3Ac"
-
+NEWS_API_KEY = "f6d056f516504ce1b83f405342ac1b50"
 CHANNEL = "@PlanenNews"
 ADMIN_ID = 8513038295
 
